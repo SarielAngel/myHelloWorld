7d899f8c96732b03f20656606a4a0cf5554df319
SarielWang <Sariel@jiyundeMacBook-Pro.local>
2018-01-05 14:07:18 +0800

    项目初建
    SSM框架，本地MySQL数据库

---
+++ ChatRobot/src/main/java/com/chatRobot/model/User.java
@@ -0,0 +1,18 @@
+package com.chatRobot.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+
+    private long id;
+    private String email;
+    private String password;
+    private String username;
+    private String role;
+    private int status;
+    private Date regTime;
+    private String regIp;
+}
